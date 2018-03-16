f940149e8c85ad5648f7fb292e3c59eccff16572
Ali Turfah <aturfah@umich.edu>
2018-03-16 12:33:53 -0400

    Add function comments

--- cmi_gui/helpers/invalid_C.m
+++ cmi_gui/helpers/invalid_C.m
@@ -1,6 +1,7 @@
 function invalid = invalid_C(C)
-%INVALID_C Summary of this function goes here
-%   Detailed explanation goes here
+%INVALID_C Check if C has been set
+%   Since C is a cell array, this can be done
+%   with isempty().
 invalid = isempty(C);
 if invalid
     debug_print("You must set C.");
